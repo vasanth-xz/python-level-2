ccc9c8345d8a43fb2620c0ce38b438afb7950d54
vasanth-xz <vasanthsaravanan357@gmail.com>
2025-07-31 14:22:58 +0530

    program 6

---
+++ program 6.py
@@ -0,0 +1,14 @@
+print("Square and Rectangle")
+print("--------------------")
+A=int(input("Enter the A value:"))
+B=int(input("Enter the B value:"))
+L=int(input("Enter the L value:"))
+print("Result")
+ps=4*A
+print("perimeter of square:",ps)
+area=A*A
+print("area of square:",area)
+PR=2*(L*B)
+print("perimeter of rectangle:",PR)
+area2=L*B
+print("area of rectangle:",area2)
